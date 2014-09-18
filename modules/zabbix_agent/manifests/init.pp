ab7cf7d5934a28b0e220027536affdbcfa75e885
Antonio Costa <antonio.costa@abril.com.br>
2014-09-18 13:04:58 +0000

    configure and add the new zabbix agent recipe

--- modules/zabbix_agent/manifests/init.pp
+++ modules/zabbix_agent/manifests/init.pp
@@ -1,2 +1,29 @@
 class zabbix_agent {
+  yumrepo {
+    'zabbix':
+      baseurl  => "http://repo.zabbix.com/zabbix/2.2/rhel/${operatingsystemmajrelease}/${architecture}/",
+      enabled  => 1,
+      gpgcheck => 0
+  }
+
+  package {
+    'zabbix-agent':
+      ensure  =>  "2.2.5-1.el${operatingsystemmajrelease}",
+      require => Yumrepo['zabbix']
+  }
+
+  file {
+    '/etc/zabbix/zabbix_agentd.conf':
+      ensure  => present,
+      owner   => 'root',
+      group   => 'root',
+      source  => 'puppet:///modules/zabbix_agent/zabbix_agentd.conf',
+      require => Package['zabbix-agent']
+  }
+
+  service {
+    'zabbix-agent':
+      ensure  => running,
+      require => File['/etc/zabbix/zabbix_agentd.conf']
+  }
 }
